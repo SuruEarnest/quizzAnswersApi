61b67dbc53dd90325e329ed09eaed7e2da37ca53
earnestsuru <serihbrah@gmail.com>
2022-01-07 22:33:41 +0100

    added DTO to recieve data from controller layer

---
+++ src/main/java/com/odysseyenergysolutions/exercise/quiz/model/dto/QuizAnswerRequestDto.java
@@ -0,0 +1,23 @@
+package com.odysseyenergysolutions.exercise.quiz.model.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class QuizAnswerRequestDto {
+
+    @NotEmpty(message = "Please specify an answer for question 1")
+    private String question1;
+    @NotNull(message ="Please specify true or false for question 2")
+    private Boolean question2;
+    @NotNull(message ="Answer to question 3 is required")
+    private Integer question3;
+    private String user;
+}
