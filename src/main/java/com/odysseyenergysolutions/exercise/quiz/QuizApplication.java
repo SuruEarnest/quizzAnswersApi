9cfbefd7b1a3c5571f35dd9694f762dc35a7846f
earnestsuru <serihbrah@gmail.com>
2022-01-07 22:27:30 +0100

    Created Main Class

--- src/main/java/com/odysseyenergysolutions/exercise/quiz/QuizApplication.java
+++ src/main/java/com/odysseyenergysolutions/exercise/quiz/QuizApplication.java
@@ -1,10 +1,12 @@
 package com.odysseyenergysolutions.exercise.quiz;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@Slf4j
 @EnableJpaAuditing
 public class QuizApplication {
   public static void main(String[] args) {
